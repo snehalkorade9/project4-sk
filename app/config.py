2632a9a3d7ce2dfe750ec5d0f1201d72c0448b6d
snehalkorade9 <snehal.korade99@gmail.com>
2022-05-04 00:09:52 +0530

    CSRF enabled set to false

--- app/config.py
+++ app/config.py
@@ -11,7 +11,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR,'..', DB_DIR, "db2.sqlite")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER =  os.getenv('UPLOAD_FOLDER', BASE_DIR + '/uploads')
-    GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY','NOKEY')
+    #GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY','NOKEY')
     LOG_DIR =  os.path.join(BASE_DIR, '../logs')
 class ProductionConfig(Config):
     pass
@@ -27,6 +27,6 @@ class TestingConfig(Config):
     SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
     SESSION_COOKIE_SECURE = False
     DEBUG = True
-    WTF_CSRF_METHOD = []
-    WTF_CSRF_ENABLED = False
+    #WTF_CSRF_METHOD = []
+    #WTF_CSRF_ENABLED = False
 
