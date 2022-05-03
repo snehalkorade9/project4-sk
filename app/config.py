fce0e8f666927a9f084cd76455661231f5c2ecc0
snehalkorade9 <snehal.korade99@gmail.com>
2022-05-04 00:55:01 +0530

    debuging csrf issue conf file

--- app/config.py
+++ app/config.py
@@ -4,14 +4,14 @@ class Config(object):
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     DEBUG = False
     TESTING = False
-    SECRET_KEY = 'This is an INSECURE secret!! DO NOT use this in production!!!'
+    SECRET_KEY = 'This is an INSECURE secret!! DO NOT use this in production!!'
     SESSION_COOKIE_SECURE = True
     BOOTSTRAP_BOOTSWATCH_THEME = 'Simplex'
     DB_DIR = os.getenv('DB_DIR','database')
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR,'..', DB_DIR, "db2.sqlite")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER =  os.getenv('UPLOAD_FOLDER', BASE_DIR + '/uploads')
-    #GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY','NOKEY')
+    GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY','NOKEY')
     LOG_DIR =  os.path.join(BASE_DIR, '../logs')
 class ProductionConfig(Config):
     pass
@@ -20,8 +20,6 @@ class ProductionConfig(Config):
 class DevelopmentConfig(Config):
     DEBUG = True
     SESSION_COOKIE_SECURE = False
-    WTF_CSRF_METHOD = []
-    WTF_CSRF_ENABLED = False
 
 
 class TestingConfig(Config):
@@ -31,4 +29,3 @@ class TestingConfig(Config):
     DEBUG = True
     WTF_CSRF_METHOD = []
     WTF_CSRF_ENABLED = False
-
