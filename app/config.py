2e091d5844d55bfd82fd27842028f67b81e03390
snehalkorade9 <snehal.korade99@gmail.com>
2022-05-04 11:31:58 +0530

    debuging csrf issue conf file updated dev

--- app/config.py
+++ app/config.py
@@ -10,9 +10,9 @@ class Config(object):
     DB_DIR = os.getenv('DB_DIR','database')
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR,'..', DB_DIR, "db2.sqlite")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER =  os.getenv('UPLOAD_FOLDER', BASE_DIR + '/uploads')
-    GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY','NOKEY')
-    LOG_DIR =  os.path.join(BASE_DIR, '../logs')
+    UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER', BASE_DIR + '/uploads')
+    #GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY','NOKEY')
+    LOG_DIR = os.path.join(BASE_DIR, '../logs')
 class ProductionConfig(Config):
     pass
 
