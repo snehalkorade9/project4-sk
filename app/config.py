54242a3c59d3c030a0a84a25693d9f76a715237e
snehalkorade9 <snehal.korade99@gmail.com>
2022-05-09 16:13:16 +0530

    Push latest code update config.py

--- app/config.py
+++ app/config.py
@@ -5,15 +5,15 @@ class Config(object):
     DEBUG = False
     TESTING = False
     SECRET_KEY = 'This is an INSECURE secret!! DO NOT use this in production!!'
-    WTF_CSRF_METHODS= ['POST', 'PUT', 'PATCH']
     SESSION_COOKIE_SECURE = True
     BOOTSTRAP_BOOTSWATCH_THEME = 'Simplex'
-    DB_DIR = os.getenv('DB_DIR','database')
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR,'..', DB_DIR, "db2.sqlite")
+    DB_DIR = os.getenv('DB_DIR', 'database')
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, '..', DB_DIR, "db2.sqlite")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER =  os.getenv('UPLOAD_FOLDER', BASE_DIR + '/uploads')
-    GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY','NOKEY')
-    LOG_DIR =  os.path.join(BASE_DIR, '../logs')
+    UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER', BASE_DIR + '/uploads')
+    GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY', 'NOKEY')
+    LOG_DIR = os.path.join(BASE_DIR, '../logs')
+
 class ProductionConfig(Config):
     pass
 
