5f608621688b556c6a1da599d4d060fbd9bbc120
snehalkorade9 <snehal.korade99@gmail.com>
2022-05-09 16:00:07 +0530

    Push latest code update config.py

--- app/config.py
+++ app/config.py
@@ -5,6 +5,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     SECRET_KEY = 'This is an INSECURE secret!! DO NOT use this in production!!'
+    WTF_CSRF_METHODS= ['POST', 'PUT', 'PATCH']
     SESSION_COOKIE_SECURE = True
     BOOTSTRAP_BOOTSWATCH_THEME = 'Simplex'
     DB_DIR = os.getenv('DB_DIR','database')
