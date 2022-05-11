6ff05d1ae51bdfdfcecc6a13f1d1a7b5229217a2
snehalkorade9 <snehal.korade99@gmail.com>
2022-05-12 02:34:48 +0530

    Test error on trying to creat existing user

--- tests/manage_user_account_test.py
+++ tests/manage_user_account_test.py
@@ -28,3 +28,17 @@ def test_new_user_creation(client, application, logged_in_user):
         assert b'Congratulations, you just created a u' in rv.data
 
 
+
+@auth.route('/users/new', methods=['POST', 'GET'])
+def test_new_user_exist(client, application, logged_in_user):
+    response = client.get("/users/new")
+    assert response.status_code == 200
+    with application.app_context():
+        rv = client.post('/users/new', data=dict(
+            email='sk@njit.edu',
+            password='Test123#',
+            confirm='Test123#'
+        ), follow_redirects=True)
+        print("rv.request.path", rv.request.path)
+        assert rv.status_code == 200
+        assert b'Already Registered' in rv.data
