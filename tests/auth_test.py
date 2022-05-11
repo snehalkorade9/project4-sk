baa8e626b9ebeabdca70df0395c653d5f7c36270
snehalkorade9 <snehal.korade99@gmail.com>
2022-05-12 00:50:09 +0530

    Test denying access to csv folder

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -8,3 +8,9 @@ def test_allow_upload_file(client, application, logged_in_user):
         assert response.status_code == 200
 
 
+@transaction.route('/transactions/upload')
+def test_deney_access_upload_file(client, application, logged_in_non_admin_user):
+    with application.app_context():
+        response = client.get("/transaction/upload")
+        #print("loggen in", response.data)
+        assert response.status_code == 403
