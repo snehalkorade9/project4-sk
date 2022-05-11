6216a601fb26ec5a07364b5ea2f6c5f8f3339653
snehalkorade9 <snehal.korade99@gmail.com>
2022-05-12 01:23:45 +0530

    Test CSV folder creation

--- tests/csv_file_test.py
+++ tests/csv_file_test.py
@@ -1,3 +1,8 @@
+import os
+
+from flask import current_app
+from werkzeug.utils import secure_filename
+
 from app import transaction
 
 @transaction.route('/transactions/upload')
@@ -14,3 +19,11 @@ def test_deney_access_upload_file(client, application, logged_in_non_admin_user)
         response = client.get("/transaction/upload")
         #print("loggen in", response.data)
         assert response.status_code == 403
+
+
+def test_log_folder_creation():
+    """check if info.log is created"""
+    root = os.path.dirname(os.path.abspath(__file__))
+    print (os.path.join(root, "/Project4/app/uploads"))
+    assert os.path.exists(os.path.join(root, "../app/uploads")) == True
+
