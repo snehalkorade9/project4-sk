b93aa0e293e66f1e6d700f2dbadba59917a5f8ff
snehalkorade9 <snehal.korade99@gmail.com>
2022-05-12 02:21:37 +0530

    Test display users in user managment

--- tests/user_managment_test.py
+++ tests/user_managment_test.py
@@ -5,6 +5,7 @@ from app import transaction, auth
 @auth.route('/users', methods=['POST', 'GET'])
 def test_user_display(client, application, logged_in_user):
     response = client.get("/users")
-    assert b"sk@njit.edu" in response.
+    assert b"sk@njit.edu" in response.data
+
 
 
